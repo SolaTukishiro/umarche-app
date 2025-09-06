a58914cbffc235859d5c0dc1228490f8f50c1e2a
TakumaAndo MBP <antaku0514@icloud.com>
2025-09-06 13:07:46 +0900

    Shop追加

--- app/Models/Owner.php
+++ app/Models/Owner.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\Shop;
 
 class Owner extends Authenticatable
 {
@@ -44,5 +45,9 @@ class Owner extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function shop(){
+        return $this->hasOne(Shop::class);
     }
 }
