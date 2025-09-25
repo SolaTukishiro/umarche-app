1ea376bf96855360258f6ab50a57996ce2f9005c
TakumaAndo MBP <antaku0514@icloud.com>
2025-09-25 20:02:10 +0900

    image index

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -3,16 +3,32 @@
 namespace App\Http\Controllers\Owner;
 
 use App\Http\Controllers\Controller;
+use App\Models\Image;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ImageController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    public function __construct(){
+        $this->middleware('auth:owners');
+
+        $this->middleware(function($request, $next){
+            $id = $request->route()->parameter('image');
+            if(!is_null($id)){
+                $imagesOwnerId = Image::findorfail($id)->owner->id;
+                $imageId = (int)$imagesOwnerId;
+                if($imageId !== Auth::id()){
+                    abort(404);
+                }
+            }
+            return $next($request);
+        });
+    }
     public function index()
     {
-        //
+        $images = Image::where('owner_id', Auth::id())->orderBy('updated_at', 'desc')->paginate(20);
+
+        return view('owner.images.index', compact('images'));
     }
 
     /**
