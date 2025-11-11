ab456f8365e4848f1f0ee5afa86df94921c227c8
TakumaAndo MBP <antaku0514@icloud.com>
2025-11-11 14:27:56 +0900

    micromodal

--- resources/views/owner/products/create.blade.php
+++ resources/views/owner/products/create.blade.php
@@ -1,42 +1,65 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <x-auth-validation-errors class="mb-4" :errors="$errors"/>
-                    <form action="{{route('owner.products.store')}}" method="post">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    <x-auth-validation-errors class="mb-4" :errors="$errors" />
+                    <form method="post" action="{{ route('owner.products.store')}}" >
                         @csrf
                         <div class="-m-2">
                             <div class="p-2 w-1/2 mx-auto">
                                 <div class="relative">
                                     <select name="category">
                                         @foreach($categories as $category)
-                                            <optgroup label="{{$category->name}}">
+                                            <optgroup label="{{ $category->name }}">
                                                 @foreach($category->secondary as $secondary)
-                                                    <option value="{{$secondary->id}}">
-                                                        {{$secondary->name}}
+                                                    <option value="{{ $secondary->id}}" >
+                                                        {{ $secondary->name }}
                                                     </option>
                                                 @endforeach
-                                            </optgroup>
                                         @endforeach
                                     </select>
+
                                 </div>
                             </div>
+                            <x-select-image :images="$images" name="image1" />
+                            <x-select-image :images="$images" name="image2" />
+                            <x-select-image :images="$images" name="image3" />
+                            <x-select-image :images="$images" name="image4" />
 
-                        </div>
-                        <div class="p-2 w-full mt-4 flex justify-around">
-                            <button type="button" onclick="location.href='{{route('owner.products.index')}}'" class="flex mx-auto bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
-                            <button type="submit" class="flex mx-auto text-white bg-blue-500 border-0 py-2 px-8 focus:outline-none hover:bg-blue-600 rounded text-lg">登録する</button>
+                            <div class="p-2 w-full flex justify-around mt-4">
+                                <button type="button" onclick="location.href='{{ route('owner.products.index')}}'" class="bg-gray-200 border-0 py-2 px-8 focus:outline-none hover:bg-gray-400 rounded text-lg">戻る</button>
+                                <button type="submit" class="text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">登録する</button>
+                            </div>
                         </div>
                     </form>
                 </div>
             </div>
         </div>
     </div>
+    <script>
+        'use strict'
+        const images = document.querySelectorAll('.image')
+
+        images.forEach( image =>  {
+            image.addEventListener('click', function(e){
+                const imageName = e.target.dataset.id.substr(0, 6)
+                const imageId = e.target.dataset.id.replace(imageName + '_', '')
+                const imageFile = e.target.dataset.file
+                const imagePath = e.target.dataset.path
+                const modal = e.target.dataset.modal
+                document.getElementById(imageName + '_thumbnail').src = imagePath + '/' + imageFile
+                document.getElementById(imageName + '_hidden').value = imageId
+                MicroModal.close(modal);
+            }, )
+        })
+
+    </script>
+
 </x-app-layout>
