55209ece7ff4341542b0d8a9a2b531b1cc7cc307
TakumaAndo MBP <antaku0514@icloud.com>
2025-11-24 05:56:39 +0900

    view 調整

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended('/items');
+        return redirect()->intended('/');
     }
 
     /**
